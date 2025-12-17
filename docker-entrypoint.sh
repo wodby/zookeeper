0870bce9bb044e64e7e6f43b0c465448ffe34947
Chingis Sandanov <chingis@chingis.io>
2025-12-17 12:45:50 +0800

    Try to fetch my id from pod hostname

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,7 +15,15 @@ _gotpl() {
 _gotpl "zoo.cfg.tmpl" "/conf/zoo.cfg"
 
 if [[ ! -f "$ZOO_DATA_DIR/myid" ]]; then
-    echo "${ZOO_MY_ID:-1}" > "$ZOO_DATA_DIR/myid"
+    HOSTNAME="$(hostname -s)"
+    if [[ $HOSTNAME =~ (.*)-([0-9]+)$ ]]; then
+        ORD=${BASH_REMATCH[2]}
+        ZOO_MY_ID="$((ORD + 1 ))"
+        echo "${ZOO_MY_ID}" > "$ZOO_DATA_DIR/myid"
+    else
+        echo "Failed to get ID from hostname $HOSTNAME, using 1 as default"
+        echo "${ZOO_MY_ID:-1}" > "$ZOO_DATA_DIR/myid"
+    fi
 fi
 
 sudo init_container
